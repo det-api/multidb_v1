ea856e6c96bfc138a08553eaaf07ab1c64fe4f7d
lwinmin-12 <lmo.lwinmin@gmail.com>
2023-09-12 17:23:39 +0630

    sec

--- config/default.ts
+++ config/default.ts
@@ -1,9 +1,9 @@
 export default {
   port: 9000,
   host: "localhost",
-  controlDbUrl: "mongodb://127.0.0.1:27017/control",
-  kyawsan_DbUrl: "mongodb+srv://detfuelstation:asdffdsa-4580@fms.dvyvwbs.mongodb.net/cloud_pos",
-  common_DbUrl: "mongodb://127.0.0.1:27017/common",
+  controlDbUrl: "mongodb+srv://ksdbfms:Tpd7iFyquVJrh4u8@controller.zabfxgj.mongodb.net/?retryWrites=true&w=majority",
+  kyawsan_DbUrl: "mongodb+srv://ksdbfms:Asdffdsa-4580@ksfms.tfk72hu.mongodb.net/?retryWrites=true&w=majority",
+  common_DbUrl: "mongodb+srv://ksdbfms:C9GADG2k0tLRl95C@common.advclns.mongodb.net/?retryWrites=true&w=majority",
   saltWorkFactor: 10,
   secretKey: "suuhh",
   page_limit: 50,
